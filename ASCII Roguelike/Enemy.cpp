05c2a99b417316478062b71225720f0e466f2916
Kir Neboula <newbie13xd@outlook.com>
2018-03-26 23:35:22 +0200

    Added variables and methods

--- ASCII Roguelike/Enemy.cpp
+++ ASCII Roguelike/Enemy.cpp
@@ -5,3 +5,36 @@ Enemy::Enemy() {
 
 
 }
+
+int Enemy::modEnemyHealth(int mod) {
+
+	if (_health + mod > _maxHealth) {
+
+		//remove excess heal
+
+		int overflowTemp = (_health + mod) - _maxHealth;
+
+		mod -= overflowTemp;
+
+	}
+	else if (_health + mod < _health) {
+
+		//player dies
+
+		if (_health < mod) {
+
+			//brutal death, etc.
+
+			return 2;
+
+		}
+
+		return 1;
+
+	}
+
+	_health += mod;
+
+	return 0;
+
+}
--- ASCII Roguelike/Enemy.h
+++ ASCII Roguelike/Enemy.h
@@ -1,7 +1,24 @@
 #pragma once
-class Enemy
-{
+
+class Enemy {
+
 public:
+
 	Enemy();
+
+	int modEnemyHealth(int mod);
+
+	int getEnemyX() { return _enemyX; };
+	int getEnemyY() { return _enemyY; };
+	void setEnemyX(int x) { _enemyX = x; };
+	void setEnemyY(int y) { _enemyY = y; };
+
+private:
+
+	int _enemyX;
+	int _enemyY;
+	int _health;
+	int _maxHealth;
+
 };
 
